260373300bc643c9ec68ba04a708d244d4c8dc6e
coinprojectz <94150446+coinprojectz@users.noreply.github.com>
2023-08-29 05:52:09 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('
-             <h1>Hello World!</h1>
+             YO
 ');
     res.end();
 }).listen(process.env.PORT || 3000);
